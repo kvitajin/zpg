f9115d34f333148f60091bf19f5c485b7dd369f0
Jindřich Kvita <jindrich.kvita@gnj.cz>
2019-10-22 17:29:04 +0200

    dohaneni cvik cv1

---
+++ cv/cmake-build-debug/CMakeFiles/cv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv.dir/main.cpp.o"
+  "cv"
+  "cv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cv/cmake-build-debug/CMakeFiles/files.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/files.dir/files/main.cpp.o"
+  "files"
+  "files.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
