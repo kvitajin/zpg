03c38a37c79cb60f2b029c06507ef135331f9440
Jindřich Kvita <jindrich.kvita@gnj.cz>
2019-10-22 22:23:15 +0200

    konec druheho cvika

--- cv/files/main.cpp
+++ cv/files/main.cpp
@@ -1,3 +1,10 @@
+
+//Include the standard C++ headers
+#include <stdlib.h>
+#include <stdio.h>
+//Include GLEW
+#include <GL/glew.h>
+
 //Include GLFW
 #include <GLFW/glfw3.h>
 
@@ -7,10 +14,6 @@
 #include <glm/mat4x4.hpp> // glm::mat4
 #include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 #include <glm/gtc/type_ptr.hpp> // glm::value_ptr
-
-//Include the standard C++ headers
-#include <stdlib.h>
-#include <stdio.h>
 
 
 
@@ -55,8 +58,13 @@ glm::mat4 View = glm::lookAt(
 glm::mat4 Model = glm::mat4(1.0f);
 
 
+
+
+
+
 int main(void)
 {
+    /*
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
 
@@ -91,8 +99,6 @@ int main(void)
     glViewport(0, 0, width, height);
 
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
     glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
 
 
@@ -102,16 +108,8 @@ int main(void)
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
+
         glfwSwapBuffers(window);
 
         glfwPollEvents();
@@ -119,4 +117,122 @@ int main(void)
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
+     */
+
+    float points[] = {
+            0.0f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f
+    };
+
+    const char* vertex_shader =
+            "#version 330\n"
+            "layout(location=0) in vec3 vp;"
+            "void main () {"
+            "     gl_Position = vec4 (vp, 1.0);"
+            "}";
+
+    const char* fragment_shader =
+            "#version 330\n"
+            "out vec4 frag_colour;"
+            "void main () {"
+            "     frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+            "}";
+
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        exit(EXIT_FAILURE);
+    }
+
+glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//
+    window = glfwCreateWindow(800, 600, "ZPG", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+// start GLEW extension handler
+    glewExperimental = GL_TRUE;
+    glewInit();
+// get version info
+    printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+    printf("Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    printf("Vendor %s\n", glGetString(GL_VENDOR));
+    printf("Renderer %s\n", glGetString(GL_RENDERER));
+    printf("GLSL %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+    printf("Using GLFW %i.%i.%i\n", major, minor, revision);
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    float ratio = width / (float)height;
+    glViewport(0, 0, width, height);
+
+    //vertex buffer object (VBO)
+    GLuint VBO = 0;
+    glGenBuffers(1, &VBO); // generate the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof (points), points,
+                 GL_STATIC_DRAW);
+//vertex attribute object(VAO)
+    GLuint VAO = 0;
+    glGenVertexArrays(1, &VAO); //generate the VAO
+    glBindVertexArray(VAO); //bind the VAO
+    glEnableVertexAttribArray(0); //enable vertex attributes
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    //create and compile shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertex_shader, NULL);
+    glCompileShader(vertexShader);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+    glCompileShader(fragmentShader);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vertexShader);
+    glLinkProgram(shaderProgram);
+
+    GLint status;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        GLint infoLogLength;
+        glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+        glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
+        fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
+
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // clear color and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        // draw triangles
+        glDrawArrays(GL_TRIANGLES, 0, 3); //mode,first,count
+        // update other events like input handling
+        glfwPollEvents();
+        // put the stuff we’ve been drawing onto the display
+        glfwSwapBuffers(window);
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+
+
+
+
 }
